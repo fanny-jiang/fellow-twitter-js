bb09eacf7568c24a5f4a0562f322622fec751875
Fanny Jiang <jiangf@bu.edu>
2017-06-12 13:04:47 -0400

    added stylesheet and html, need to route static files

--- app.js
+++ app.js
@@ -2,39 +2,43 @@ const express = require('express')
 const app = express()
 
 const morgan = require('morgan')
-
 const nunjucks = require('nunjucks')
 
-const path = require('path')
+const routes = require('./routes')
 
 app.set('view engine', 'html') // have res.render work with html files
 app.engine('html', nunjucks.render) // when giving html files to res.render, tell it to use nunjucks
 nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
-
 app.use(morgan('dev'))
+
+app.use('/', routes)
 
 // app.get('/', function(req, res) {
 //   res.send('welcome')
 // })
 
-app.get('/', function(req, res) {
-  const people = [{ name: 'Full' }, { name: 'Stacker' }, { name: 'Son' }]
-  res.render('index', { title: 'Hall of Fame', people: people })
-})
+/* vvvv Nunjucks HTML render example vvvv */
 
-var locals = {
-    title: 'An Example',
-    people: [
-        { name: 'Gandalf'},
-        { name: 'Frodo' },
-        { name: 'Hermione'}
-    ]
-}
+// app.get('/', function(req, res) {
+//   const people = [{ name: 'Full' }, { name: 'Stacker' }, { name: 'Son' }]
+//   res.render('index', { title: 'Hall of Fame', people: people })
+// })
 
-nunjucks.render('index.html', locals, function(err, output) {
-    if (err) return console.error(err);
-    console.log(output);
-})
+// var locals = {
+//     title: 'An Example',
+//     people: [
+//         { name: 'Gandalf'},
+//         { name: 'Frodo' },
+//         { name: 'Hermione'}
+//     ]
+// }
+
+// nunjucks.render('index.html', locals, function(err, output) {
+//     if (err) return console.error(err);
+//     console.log(output);
+// })
+
+/* ^^^^ Nunjucks HTML render example ^^^^ */
 
 app.listen(3000, function() {
   console.log('listening on port 3000')
---
+++ tweetBank.js
@@ -0,0 +1,54 @@
+const _ = require('lodash')
+
+var data = []
+
+function add (name, content) {
+  data.push({ name: name, content: content });
+}
+
+// returns a copy of the data array, prevents mutation of the original data
+function list () {
+  return _.cloneDeep(data);
+}
+
+// looks through the array and returns an array of only the objects for which the properties passed in exist
+function find (properties) {
+  return _.cloneDeep(_.filter(data, properties));
+}
+
+module.exports = { add: add, list: list, find: find };
+
+const randArrayEl = function(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+const getFakeName = function() {
+  const fakeFirsts = ['Nimit', 'David', 'Shanna', 'Emily', 'Scott', 'Karen', 'Ben', 'Dan', 'Ashi', 'Kate', 'Omri', 'Gabriel', 'Joe', 'Geoff'];
+  const fakeLasts = ['Hashington', 'Stackson', 'McQueue', 'OLogn', 'Ternary', 'Claujure', 'Dunderproto', 'Binder', 'Docsreader', 'Ecma'];
+  return randArrayEl(fakeFirsts) + " " + randArrayEl(fakeLasts);
+};
+
+const getFakeTweet = function() {
+  const awesome_adj = ['awesome', 'breathtaking', 'amazing', 'funny', 'sweet', 'cool', 'wonderful', 'mindblowing', 'impressive'];
+  return "Fullstack Academy is " + randArrayEl(awesome_adj) + "! The instructors are just so " + randArrayEl(awesome_adj) + ". #fullstacklove #codedreams";
+};
+
+for (let i = 0; i < 10; i++) {
+  module.exports.add( getFakeName(), getFakeTweet() );
+}
+
+// _.cloneDeep(): recursively clones value or data set so that you don't mutate the actual data
+// _.filter(): Iterates over elements of collection, returning an array of all elements predicate returns truthy for.
+
+/*
+
+Test the modules:
+// add(name, content)
+add('Fanny', 'This is so cool')
+console.log('tweet data', data)
+console.log('list: ', list())
+
+var tweetBank = require('./tweetBank')
+tweetBank.find({ name: 'Scott Claujure' })
+
+*/
