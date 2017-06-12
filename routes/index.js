26b4a79399335999347cf261c91bf6a40398a214
Fanny Jiang <jiangf@bu.edu>
2017-06-12 15:09:16 -0400

    able to render single-user route and view

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,20 @@ const tweetBank = require('../tweetBank')
 
 router.get('/', function (req, res) {
   let tweets = tweetBank.list()
+  // let names = tweets.forEach((tweet) => { return tweet[name] })
+  // console.log('names: ', names)
   res.render('index', { tweets: tweets })
+})
+
+router.get('/users/:name', function(req, res) {
+  var name = req.params.name
+  var tweets = tweetBank.find( { name: name })
+  console.log('list: ', tweets)
+  res.render('index', { tweets: tweets })
+})
+
+router.get('/tweets/:id', function(req, res) {
+
 })
 
 module.exports = router
